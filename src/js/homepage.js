bc6e925382ce96d638b0b563c2e8121f0db3c3a1
beto <albertocisc@openmailbox.org>
2018-05-04 15:11:48 -0500

    Homepage layout. Not server connection nor form validation

---
+++ src/js/homepage.js
@@ -0,0 +1,11 @@
+function showSignUpSection()
+{
+	document.getElementById("login-section").style.display = "none";
+	document.getElementById("sign-up-section").style.display = "block";
+}
+
+function showLogInSection()
+{
+	document.getElementById("sign-up-section").style.display = "none";
+	document.getElementById("login-section").style.display = "block";
+}
