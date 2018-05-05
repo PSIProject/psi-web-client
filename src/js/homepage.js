bfb5aaf42fd562f7d6c5bf0a68e4224688505657
beto <albertocisc@openmailbox.org>
2018-05-05 09:59:40 -0500

    Added homepage.js code documentation

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,9 +1,16 @@
+//////////////////////////////////
+/// Hides the login section and
+/// displays the sign-up section
+//////////////////////////////////
 function showSignUpSection()
 {
 	document.getElementById("login-section").style.display = "none";
 	document.getElementById("sign-up-section").style.display = "block";
 }
-
+//////////////////////////////////
+/// Hides the sign-up section and
+/// displays the login section
+//////////////////////////////////
 function showLogInSection()
 {
 	document.getElementById("sign-up-section").style.display = "none";
