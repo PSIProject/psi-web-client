e9a9aa8c54be2f6cd1b183233d65f08e36d0a151
beto <albertocisc@openmailbox.org>
2018-05-09 09:34:01 -0500

    New section: Add member section; Searching users works

--- src/js/team-tasks.js
+++ src/js/team-tasks.js
@@ -5,6 +5,7 @@
 //////////////////////////////////
 const Sections =
 {
+	AddMember:  "add-member-section",
 	Home:		"home-section",
 	NewGoal:	"new-goal-section",
 	NewTeam:	"new-team-section",
@@ -325,6 +326,43 @@ function getTeamMembers()
 	}
 	xhr.open("GET", "php/get-team-members.php");
 	xhr.send();
+}
+
+//////////////////////////////////
+/// Search all the users which nick
+/// seems like the value of search-user-nick
+/// field. Displays them in a list
+//////////////////////////////////
+function searchUsers()
+{
+	var keyword = document.forms ["search-user-form"] ["search-user-nick"].value;
+
+	var foundUsersArea = document.getElementById("found-users-area");
+	while (foundUsersArea.firstChild)
+		foundUsersArea.removeChild(foundUsersArea.firstChild);
+
+	if (! keyword.length)
+		return;
+
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function()
+	{
+		if (this.readyState != 4 || this.status != 200)
+			return;
+
+		var foundUsers = JSON.parse(this.responseText);
+		for (var i = 0; i < foundUsers.length; i++)
+		{
+			var foundUser = foundUsers [i];
+			var userElement = document.createElement("div");
+			userElement.className = "scroll-area-row text-button";
+			userElement.innerHTML = foundUser.nick;
+			foundUsersArea.appendChild(userElement);
+		}
+	}
+	xhr.open("POST", "php/search-users.php");
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send("keyword=" + keyword);
 }
 
 //////////////////////////////////
